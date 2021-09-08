172ea674455afe6a4a87b7555c869cf973a441d4
Vladimir-Runov <9322304@gmail.com>
2021-09-08 19:38:36 +0300

    build ok!

---
+++ src/test/java/ru/gb/spring_p3/SpringP3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.spring_p3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringP3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
