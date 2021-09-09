17bf560ca50dddde2adfa0302b38e8244466f88d
Vladimir-Runov <9322304@gmail.com>
2021-09-09 22:45:04 +0300

    fixes

--- ms-products/src/test/java/ru/gb/products/ProdustControllerTest.java
+++ ms-products/src/test/java/ru/gb/products/ProdustControllerTest.java
@@ -5,16 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-
+//@ActiveProfiles("test")
 public class ProdustControllerTest {
     @Autowired
     private MockMvc mvc;
@@ -25,9 +28,9 @@ public class ProdustControllerTest {
             this.mvc.perform(get("/api/v1/products").contentType(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
                     .andExpect((ResultMatcher) jsonPath("$.content").isArray())
-                    .andExpect(jsonPath("$.content".hasSize(3)))
-                    .andExpect(jsonPath("$.content[0].title", is("Bread")));
-
+                    .andExpect((ResultMatcher) jsonPath("$.content", hasSize(3)))
+                    .andExpect((ResultMatcher) jsonPath("$.content[0].title", is("Bread")));
+///  почему-то без (ResultMatcher) не хочет...
 
     }
 }
